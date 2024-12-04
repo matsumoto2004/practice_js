83655baca4161ff83c687ebc391bec6ed90bf3dd
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-04 23:41:23 +0900

    async#6 finished

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=a_8nrslImo4&list=PL4cUxeGkcC9jx2TTZk3IGWKSbtugYdrlu
+
+この動画をもとにjavascriptの勉強
--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,23 @@
-const para = document.querySelector("p");
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener('readystatechange',()=>{
+    if(request.readyState === 4 && request.status === 200){
+      const data=JSON.parse(request.responseText);
+      callback(undefined,data);
+    }
+  });
+
+  request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+  request.send();
+};
+
+getTodos((err,data)=> {
+  console.log("callback fired");
+  if(err){
+    console.log(err);
+  }else{
+    console.log(data);
+  }
+});
+
