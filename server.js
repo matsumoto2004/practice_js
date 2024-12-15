9037b4ac2f683acb19bb46efd31aa8281f5332e8
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-15 13:29:29 +0900

    #5 finished

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http=require("http");
 const fs=require("fs");
+const _=require("lodash")
 
 const server=http.createServer((req,res)=>{
+  //lodash
+  const num= _.random(0,20);
+  console.log(num);
+
+  const greet= _.once(()=>{
+    console.log("hello");
+  })
+  greet();
+  greet();
   console.log(req.url,req.method);
   res.setHeader("Content-type","text/html");
 
