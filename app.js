50cc2dff164dbfdb2308fc27cc11609dfd77153f
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-15 15:23:33 +0900

    #7 ejs view engine

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine","ejs");
+
 //listen for requests
 app.listen(3000);
 
 app.get("/",(req,res)=>{
-  //res.send("<p>home page</p>");
-  res.sendFile("./views/index.html",{root:__dirname});
+  res.render("index");
 });
 
 app.get("/about",(req,res)=>{
-  //res.send("<p>about page</p>");
-  res.sendFile("./views/about.html",{root:__dirname});
+  res.render("about");
 });
 
-app.get("/about-us",(req,res)=>{
-  res.redirect("/about");
-});
+app.get("/blogs/create",(req,res)=>{
+  res.render("create");
+})
 
 //これは最後にすべき
 app.use((req,res)=>{
-  res.status(404).sendFile("./views/404.html",{root:__dirname})
+  res.status(404).render("404");
 });
