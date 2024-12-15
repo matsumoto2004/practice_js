bbe7a00a6338cf30dadbec3f43873c53fba7e1b3
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-15 16:51:26 +0900

    #8 finished

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -6,6 +7,9 @@ app.set("view engine","ejs");
 
 //listen for requests
 app.listen(3000);
+
+app.use(express.static("public"))
+app.use(morgan("dev"));
 
 app.get("/",(req,res)=>{
   const blogs=[
