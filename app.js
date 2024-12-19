10dd9c78e1381ff6fd9bbfe0d38ee961497b22ef
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-19 21:57:02 +0900

    #9 finished

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const app = express();
 
+const dbURI="mongodb+srv://matsumoto11:ZZ_T37j4auegLJ7@cluster0.wmf1f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(dbURI)
+.then((result)=>{
+  app.listen(3000);
+})
+.catch((err)=>{
+  console.log(err);
+});
+
 app.set("view engine","ejs");
 
-//listen for requests
-app.listen(3000);
+app.get("/add-blog",(req,res)=>{
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog"
+  });
+  blog.save()
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+});
+
+app.get("/all-blogs",(req,res)=>{
+  Blog.find()
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+});
+
+app.get("/single-blog",(req,res)=>{
+  Blog.findById("675ede580ae1a178a9592be3")
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
 
 app.use(express.static("public"))
 app.use(morgan("dev"));
