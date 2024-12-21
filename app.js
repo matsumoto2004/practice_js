570628108c78e9ac3b51c9e25b0d46a65bce755b
matsumoto2004 <mamamaturi57@gmail.com>
2024-12-21 15:24:32 +0900

    #11 finished

--- app.js
+++ app.js
@@ -16,60 +16,70 @@ mongoose.connect(dbURI)
 
 app.set("view engine","ejs");
 
-app.get("/add-blog",(req,res)=>{
-  const blog = new Blog({
-    title: "new blog",
-    snippet: "about my new blog",
-    body: "more about my new blog"
-  });
-  blog.save()
-  .then((result)=>{
-    res.send(result);
-  })
-  .catch((err)=>{
-    console.log(err);
-  });
-});
-
-app.get("/all-blogs",(req,res)=>{
-  Blog.find()
-  .then((result)=>{
-    res.send(result);
-  })
-  .catch((err)=>{
-    console.log(err);
-  });
-});
-
-app.get("/single-blog",(req,res)=>{
-  Blog.findById("675ede580ae1a178a9592be3")
-  .then((result)=>{
-    res.send(result);
-  })
-  .catch((err)=>{
-    console.log(err);
-  })
-})
-
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
 app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
 app.get("/",(req,res)=>{
-  const blogs=[
-    // {title:"blog1",snippet:"snip1"},
-    // {title:"blog2",snippet:"snip2"},
-    // {title:"blog3",snippet:"snip3"}
-  ];
-  res.render("index",{title:"Home",blogs});
+  res.redirect('/blogs');
 });
 
 app.get("/about",(req,res)=>{
   res.render("about",{title:"About"});
 });
 
+app.get("/blogs",(req,res)=>{
+  Blog.find().sort({ createdAt: -1 })
+  .then(result => {
+    res.render('index', { blogs: result, title: 'All blogs' });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+app.post("/blogs",(req,res)=>{
+  const blog = new Blog(req.body);
+
+  blog.save()
+  .then((result)=>{
+    res.redirect("/blogs");
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
+
 app.get("/blogs/create",(req,res)=>{
   res.render("create",{title:"Create"});
-})
+});
+
+app.get("/blogs/:id",(req,res)=>{
+  const id = req.params.id;
+  Blog.findById(id)
+  .then((result)=>{
+    res.render("details",{blog: result,title: "Blog Details"})
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+});
+
+app.delete("/blogs/:id",(req,res)=>{
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+  .then((result)=>{
+    res.json({redirect: "/blogs"});
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+});
 
 //これは最後にすべき
 app.use((req,res)=>{
